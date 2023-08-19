7472f1edc54db84ccb3cc2545619be8aeb4975c0
angli4anochka <Angli4anochka@icloud.com>
2023-08-19 05:46:41 +0300

    files added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <header className= 'header'>
+      <img src ="https://i.pinimg.com/originals/0d/46/a6/0d46a6c1a51b31711a07e7fb83f1e534.png"></img>
+    </header >
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+<div className= 'main-content'>
+<div><img src = 'https://avatars.mds.yandex.net/i?id=96efd8537f8b5eecd0d45eab2d53eaf887d3b4c1-10375548-images-thumbs&n=13'></img></div>
+<div className='avadescr'></div>
+<div className = "MyPosts"></div>
+<div className = "newPosts"></div>
+<div className='posts'></div>
+</div>
+)
+}
+
+export default Profile
